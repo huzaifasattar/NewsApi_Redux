0407eead47fb81d28444fe694329494ea91dfe71
NewsApi_Redux <huzaifasattar89@gmail.com>
2023-03-18 02:46:05 +0500

    nothing

--- src/commponents/reduxThunk/store.js
+++ src/commponents/reduxThunk/store.js
@@ -1,7 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import  reducer  from "./reducer";
+import reducer from "./reducer";
 
-const store = configureStore({
-  reducer,
-});
+const store = configureStore({ reducer });
 export default store;
